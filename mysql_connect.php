a05bcd8c8a52a34b6b2d02dd4a6743ae740b6094
alsaeedz <alsaeedz@seattleu.edu>
2013-05-20 17:58:11 -0700

    Testing select statement to get familiar with PHP

--- mysql_connect.php
+++ mysql_connect.php
@@ -9,7 +9,22 @@ $db_name = "firstplace";
 @mysql_select_db ("$db_name") or die ("No database");
 
 
+// This select statment shouldn't be here, I just was testing it. It should be in a diffrent .php file
+$sql = mysql_query ("SELECT * FROM `employee`");
 
-//$sql = mysql_query ("SELECT * FROM `table`);
+while ($row = mysql_fetch_array ($sql)){
 
+	$emp_id = $row["emp_id"];
+	$FirstName = $row["FirstName"];
+	$LastName = $row["LastName"];
+	$PhoneNumber = $row["PhoneNumber"];
+	$Address = $row["Address"];
+	$email_Address = $row["email_Address"];
+	$classroom = $row["classroom"];
+	$password = $row["password"];
+	$username = $row["username"];
+	
+	echo $emp_id . ", " . $FirstName . ", " . $LastName . ", " . $PhoneNumber . ", " . $Address . ", " . $email_Address . ", " . $classroom . ", " . $password . ", " . $username . "<br>";
+	
+	};
 ?>
