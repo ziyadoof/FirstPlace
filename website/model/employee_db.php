c6412d4e075d17d23064dca90e9ce9e157db1d83
tldavies <tldavies87@gmail.com>
2013-05-28 15:12:24 -0700

    Added employee_db

---
+++ website/model/employee_db.php
@@ -0,0 +1,24 @@
+<?php
+class StudentDB {
+    public static function getStudents() {
+       //TODO
+        return $students;
+    }
+    
+     public static function getStudentByClass($class_id) {
+         //TODO
+        return $students;
+    }
+    
+    
+    public static function getProduct($s_id) {
+        //TODO
+        return $student;
+    }
+
+    public static function addStudent($student) {
+       //TODO
+        return $row_count;
+    }
+}
+?>
