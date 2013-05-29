6ea8b5cc8dd75a4a6adebde98f261752111d4b71
Padmini <vedulap@seattleu.edu>
2013-05-28 22:05:00 -0700

    Adding attendance_db.php and attendanceType_db.php

---
+++ website/model/attendanceType_db.php
@@ -0,0 +1,29 @@
+<?php
+class AttendanceTypeDB {
+    public static function getAttendance() {
+       //TODO
+        return $students;
+    }
+    
+     public static function getAttendanceByClass($class_id) {
+         //TODO
+        return $students;
+    }
+    
+    
+    public static function getAttendanceByStudent($s_id) {
+        //TODO
+        return $student;
+    }
+
+    public static function addAttendance($student) {
+       //TODO
+        return $row_count;
+    }
+	
+	public static function updateAttendance($student) {
+       //TODO
+        return $row_count;
+    }
+}
+?>
