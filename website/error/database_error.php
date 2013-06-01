79c5136d550862bf0227dbe3d111957f781672bc
tldavies <tldavies87@gmail.com>
2013-05-31 17:27:22 -0700

    Updated Database error to not point to the book

--- website/error/database_error.php
+++ website/error/database_error.php
@@ -2,8 +2,8 @@
 <div id="main">
     <h1 class="top">Database Error</h1>
     <p>There was an error connecting to the database.</p>
-    <p>The database must be installed as described in the appendix.</p>
-    <p>MySQL must be running as described in chapter 1.</p>
+    <p>The database must be installed and running.</p>
+    <p>Please check that MySQL is running.</p>
     <p>Error message: <?php echo $error_message; ?></p>
     <p>&nbsp;</p>
 </div><!-- end main -->
