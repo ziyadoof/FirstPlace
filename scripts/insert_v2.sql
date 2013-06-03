8201fd95df9c5f4d7b9c5234a1b70d74df9fc792
Padmini <vedulap@seattleu.edu>
2013-06-03 13:37:56 -0700

    Merge branch 'master' of https://github.com/ziyadoof/FirstPlace
    Conflicts:
    	scripts/FP_script.v16 - 06-02-2013.sql
    	scripts/FirstPlace-v8 - 05-28-2013.mwb
    	scripts/FirstPlace-v8 - 05-28-2013.mwb.bak
    	website/model/schoolYear_class.php
    	website/model/schoolYear_db.php

---
+++ scripts/insert_v2.sql
@@ -0,0 +1,25 @@
+-- Insert for Roles
+INSERT INTO `firstplace`.`role` (`Role_ID`, `RoleName`) VALUES ('1', 'Admin');
+INSERT INTO `firstplace`.`role` (`Role_ID`, `RoleName`) VALUES ('2', 'Teacher');
+INSERT INTO `firstplace`.`role` (`Role_ID`, `RoleName`) VALUES ('3 ', 'Case Worker');
+
+
+
+-- Inserts for Rooms
+INSERT INTO `firstplace`.`Room` (`Location`) VALUES ('ENGR123');
+INSERT INTO `firstplace`.`Room` (`Location`) VALUES ('123');
+INSERT INTO `firstplace`.`Room` (`Location`) VALUES ('A45');
+
+
+-- insert for People
+INSERT INTO `firstplace`.`employee` (`emp_id`, `FirstName`, `LastName`, `PhoneNumber`, `email_Address`, `password`, `username`, `Role_Role_ID`) VALUES ('1', 'Tracy', 'Davies', '123', '123@123', '123', 'tracydav1', '1');
+INSERT INTO `firstplace`.`employee` (`emp_id`, `FirstName`, `LastName`, `PhoneNumber`, `email_Address`, `password`, `username`, `Role_Role_ID`) VALUES ('2', 'Pad', 'Smithq', '34', '123@123', '213', 'padsmith', '2');
+INSERT INTO `firstplace`.`employee` (`emp_id`, `FirstName`, `LastName`, `PhoneNumber`, `Address`, `email_Address`, `password`, `username`, `Role_Role_ID`) VALUES ('3', 'Ziyad', 'Joe', '23123', 'Main St', '12345@test', '213', 'ziydajoe', '3');
+
+-- insert for Students
+INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('MAry', 'Jane', '123', '123 Main', 'CURDATE()');
+INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('Joe', 'Smith', '34', 'Tree St', 'CURDATE()');
+INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('Kenny', 'Von', '32512', 'Pain', 'CURDATE()');
+
+
+
