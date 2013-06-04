04eb4658aa37f326acf745c209153681bd960a90
alsaeedz <alsaeedz@seattleu.edu>
2013-06-04 11:50:31 -0700

    updated the model (v12), new Script (v31), and insert (v2)

--- scripts/insert_v2.sql
+++ scripts/insert_v2.sql
@@ -14,18 +14,18 @@ INSERT INTO `firstplace`.`employee` (`emp_id`, `FirstName`, `LastName`, `PhoneNu
 INSERT INTO `firstplace`.`employee` (`emp_id`, `FirstName`, `LastName`, `PhoneNumber`, `Address`, `email_Address`, `password`, `username`, `Role_Role_ID`) VALUES ('3', 'Ziyad', 'Joe', '23123', 'Main St', 'ziyad@test.com', '213', 'ziydajoe', '3');
 
 -- insert for Students
-INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('MAry', 'Jane', '123', '123 Main', 'CURDATE()');
-INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('Joe', 'Smith', '34', 'Tree St', 'CURDATE()');
-INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('Kenny', 'Von', '32512', 'Pain', 'CURDATE()');
+INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('MAry', 'Jane', '123', '123 Main', '2013-05-27');
+INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('Joe', 'Smith', '34', 'Tree St', '2013-05-28');
+INSERT INTO `firstplace`.`Student` (`FirstName`, `LastName`, `PhoneNumber`, `Address`, `YearStarted`) VALUES ('Kenny', 'Von', '32512', 'Pain', '2013-05-29');
 
 -- insert for Specialty
-INSERT INTO `firstplace`.`specialty` (`Type`, `Name`, `Start_Date`) VALUES ('Training', 'Math', '2000-01-01');
-INSERT INTO `firstplace`.`specialty` (`Type`, `Name`, `Start_Date`) VALUES ('College', 'Chemistry', '2010-04-22');
+INSERT INTO `firstplace`.`specialty` (`Type`, `Name`) VALUES ('Training', 'Math');
+INSERT INTO `firstplace`.`specialty` (`Type`, `Name`) VALUES ('College', 'Chemistry');
 
 -- insert for School Year
-INSERT INTO `firstplace`.`schoolyear` (`StartDate`, `EndDate`, `Name`) VALUES ('2010', '2011', '2010/2011');
-INSERT INTO `firstplace`.`schoolyear` (`StartDate`, `EndDate`, `Name`) VALUES ('2011', '2012', '2011/2012');
-INSERT INTO `firstplace`.`schoolyear` (`StartDate`, `EndDate`, `Name`) VALUES ('2012', '2013', '2012/2013');
+INSERT INTO `firstplace`.`schoolyear` (`StartDate`, `EndDate`, `Name`) VALUES ('2010-09-21', '2011-08-10', '2010/2011');
+INSERT INTO `firstplace`.`schoolyear` (`StartDate`, `EndDate`, `Name`) VALUES ('2011-09-21', '2012-08-10', '2011/2012');
+INSERT INTO `firstplace`.`schoolyear` (`StartDate`, `EndDate`, `Name`) VALUES ('2012-09-21', '2013-08-10', '2012/2013');
 
 -- insert for Holiday
 INSERT INTO `firstplace`.`holiday` (`SchoolYear_sy_id`, `StartDate`, `EndDate`, `Name`) VALUES ('1', '2013-05-27', '2013-05-27', 'Memorial Day');
