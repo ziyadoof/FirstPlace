f963eafe141a1c12277d683efda318112b071e22
tldavies <tldavies87@gmail.com>
2013-06-04 21:51:39 -0700

    Fixed header -- sorry

--- website/view/header.php
+++ website/view/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
  	<title>First Place</title>
-	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="/FirstPlace/website/style.css"/>
         <script language="javascript" src="../calendar/calendar.js"></script>
 </head>
 
@@ -11,11 +11,11 @@
 	<!--header -->
 	<header>			
 		<div id="header">
-			<h1 id="logo-text"><a href="index.php" title="">First Place</a></h1>		
+			<h1 id="logo-text"><a href="/FirstPlace/website/index.php" title="">First Place</a></h1>		
 			<p id="slogan">"Hope, Home, and Education for every child, one family at a time"</p>	
 		</div>
 		<div id="top-menu">
-			<p><a href="index.php">Login</a> | <a href="user_profile/user_profile.php">User Profile</a> | <a href="index.php">Help</a></p>
+			<p><a href="index.php">Login</a> | <a href="/FirstPlace/website/user_profile/user_profile.php">User Profile</a> | <a href="index.php">Help</a></p>
 		</div>			
 				
 	<!--header ends-->					
@@ -26,34 +26,34 @@
 			<ul>
 				<li><a href="#">Admin</a>
 					<ul>
-						<li><a href="admin/?action=show_add_employee_form">Users Management</a></li>
-						<li><a href="roles_mgmt/?action=show_add_role_form">Roles Management</a></li>
-						<li><a href="rooms_mgmt/?action=show_add_room_form">Rooms Management</a></li>
-						<li><a href="specialty_mgmt/?action=show_add_specialty_form">Specialty Management</a></li>
-						<li><a href="school_year_mgmt/?action=show_add_schoolYear_form">School Year Management</a></li>
-						<li><a href="stdClass_mgmt/?action=show_add_standardClass_form">Standard Classes Management</a></li>
-						<li><a href="admin/view_logs.php">View Logs</a></li>
+						<li><a href="/FirstPlace/website/admin/?action=show_add_employee_form">Users Management</a></li>
+						<li><a href="/FirstPlace/website/roles_mgmt/?action=show_add_role_form">Roles Management</a></li>
+						<li><a href="/FirstPlace/website/rooms_mgmt/?action=show_add_room_form">Rooms Management</a></li>
+						<li><a href="/FirstPlace/website/specialty_mgmt/?action=show_add_specialty_form">Specialty Management</a></li>
+						<li><a href="/FirstPlace/website/school_year_mgmt/?action=show_add_schoolYear_form">School Year Management</a></li>
+						<li><a href="/FirstPlace/website/school_year_mgmt/?action=show_add_standardClass_form">Standard Classes Management</a></li>
+						<li><a href="/FirstPlace/website/admin/view_logs.php">View Logs</a></li>
 					</ul>
 				</li>
 				<li><a href="#">Students</a>
 					<ul>
-						<li><a href="student/?action=show_add_student_form">Student Management</a></li>                                            
+						<li><a href="/FirstPlace/website/student/?action=show_add_student_form">Student Management</a></li>                                            
 					</ul>
 				</li>
 				<li><a href="#">Classes</a>
 					<ul>
-						<li><a href="class/?action=show_add_class_form">Class Management</a></li>
+						<li><a href="/FirstPlace/website/class/?action=show_add_class_form">Class Management</a></li>
 						
 					</ul>
 				</li>
 				<li><a href="#">Attendance</a>
 					<ul>
-						<li><a href="attendance_take/?action=take_attendance">Take Attendance</a></li>
-						<li><a href="attendance_view/?action=view_attendance">View Attendance</a></li>
-						<li><a href="attendance_mgmt/?action=show_add_attendance_type">Attendance types Management</a></li>
+						<li><a href="/FirstPlace/website/attendance_take/?action=take_attendance">Take Attendance</a></li>
+						<li><a href="/FirstPlace/website/attendance_view/?action=view_attendance">View Attendance</a></li>
+						<li><a href="/FirstPlace/website/attendance_mgmt/?action=show_add_attendance_type">Attendance types Management</a></li>
 					</ul>
 				</li>
-				<li><a href="notification/view_notification.php">Notifications</a>
+				<li><a href="/FirstPlace/website/notification/view_notification.php">Notifications</a>
 			</ul>
 		</ul>
 	</nav>
